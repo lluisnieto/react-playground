0f97fe1f6711189de3e032eba37c8821f5381727
lluis <lluis@esl-education.org>
2017-05-04 14:23:33 +0200

    Load sample data (fishes) into the app state

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
     // getinitialState
     this.state = {
       fishes: {},
@@ -25,6 +27,12 @@ class App extends React.Component {
     this.setState({ fishes: fishes});
   }
 
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -33,7 +41,7 @@ class App extends React.Component {
         </div>
 
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
   }
