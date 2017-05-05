e188b736e6dd57d7177b99361f502a1553e13399
lluis <lluis@esl-education.org>
2017-05-05 11:47:10 +0200

    Display order state using Order component

--- src/components/App.js
+++ src/components/App.js
@@ -74,7 +74,10 @@ class App extends React.Component {
           </ul>
         </div>
 
-        <Order />
+        <Order
+			fishes={ this.state.fishes }
+			order={ this.state.order }
+		/>
         <Inventory
 			addFish={this.addFish.bind(this)}
 			loadSamples={this.loadSamples.bind(this)}
--- src/components/Order.js
+++ src/components/Order.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import {formatPrice} from '../helpers';
 
 class Order extends React.Component {
-  render() {
-    return(
-      <p>Order</p>
-    )
-  }
+
+	composeOrder(prevTotal, key) {
+		const fish = this.props.fishes[key];
+		const count = this.props.order[key];
+		const isAvailable = fish && fish.status === 'available';
+		if(isAvailable){
+			return prevTotal + (count * fish.price || 0)
+		}
+		return prevTotal;
+	}
+
+	renderOrder(key) {
+		const fish = this.props.fishes[key];
+		const count = this.props.order[key];
+
+		if(!fish || fish.status === 'unavailable') {
+			return <li key={key}>Sorry, {fish ? fish.name : ''} is no longer available!</li>
+		}
+
+		return (
+			<li key={key}>
+				<span>{count}lbs {fish.name}</span>
+				<span className="price">{formatPrice(count * fish.price)}</span>
+			</li>
+		)
+	}
+
+	render() {
+		const orderIds = Object.keys(this.props.order);
+		const total = orderIds.reduce(this.composeOrder.bind(this), 0);
+
+		return(
+			<div className="order-wrap">
+				<h2>Your Order</h2>
+				<ul className="order">
+					{orderIds.map(this.renderOrder.bind(this))}
+					<li className="total">
+						<strong>Total:</strong>
+						{formatPrice(total)}
+					</li>
+				</ul>
+				<p>{orderIds}</p>
+
+			</div>
+		)
+	}
 }
 
 export default Order;
