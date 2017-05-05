eff916f58445adfbacfb706e801bdf9a53a3d667
lluis <lluis@esl-education.org>
2017-05-05 17:02:32 +0200

    React transitions enabled

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {formatPrice} from '../helpers';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Order extends React.Component {
 
@@ -37,13 +38,20 @@ class Order extends React.Component {
 		return(
 			<div className="order-wrap">
 				<h2>Your Order</h2>
-				<ul className="order">
+				<CSSTransitionGroup
+					className="order"
+					transitionName="order"
+					transitionEnterTimeout={500}
+					transitionLeaveTimeout={500}
+					component="ul"
+					
+				>
 					{orderIds.map(this.renderOrder.bind(this))}
 					<li className="total">
 						<strong>Total:</strong>
 						{formatPrice(total)}
 					</li>
-				</ul>
+				</CSSTransitionGroup>
 
 			</div>
 		)
